3ecfd01593e5378f61711ce806512a73b15a8df6
ZhendongWu <rsshinide38@163.com>
2024-12-26 19:57:06 +0800

    Fix 404 layout

--- themes/medium/index.js
+++ themes/medium/index.js
@@ -330,15 +330,17 @@ const LayoutArchive = props => {
  * @returns
  */
 const Layout404 = props => {
-  return <LayoutBase {...props}>
-        <div className='w-full h-96 py-80 flex flex-col justify-center items-center'>未找到页面，不过别灰心这可能是由于网络问题导致
-            <div>
-                您可以尝试访问本站的
-                <a href='https://alphaboom.notion.site/alphaboom/32c16f6bb85e4e949eba49dee2d73a5c?v=e7c99abff7b6444683b94893a4f838a1' target="_blank" className='text-blue-600 hover:underline underline-offset-4'>Notion公开网页</a>
-                来寻早原始内容
-            </div>
-        </div>
-    </LayoutBase>
+  return (
+    <>
+      <div className='w-full h-96 py-80 flex flex-col justify-center items-center'>未找到页面，不过别灰心这可能是由于网络问题导致
+          <div>
+              您可以尝试访问本站的
+              <a href='https://alphaboom.notion.site/alphaboom/32c16f6bb85e4e949eba49dee2d73a5c?v=e7c99abff7b6444683b94893a4f838a1' target="_blank" className='text-blue-600 hover:underline underline-offset-4'>Notion公开网页</a>
+              来寻找原始内容
+          </div>
+      </div>
+    </>
+  )
 }
 
 /**
